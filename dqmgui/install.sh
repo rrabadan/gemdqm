34753bb521ebc7b353dc25d5205fa30ccf1c4285
Raul Rabadan <raul.iraq.rabadan.trejo@cern.ch>
2023-02-23 17:06:39 +0100

    add 'manage start' to dqmgui/intall.sh

--- dqmgui/install.sh
+++ dqmgui/install.sh
@@ -30,4 +30,10 @@ echo $PWD
 
 $PWD/deployment/Deploy -A $ARCH -r "comp=comp" -R comp@$VER -t MYDEV -s "prep sw post" $PWD dqmgui/bare
 
+source current/apps/dqmgui/128/etc/profile.d/env.sh
+sed -i -e '/export keytab/,+7 s/^/## /' $PWD/current/config/dqmgui/manage
+#sed -i -e 's/127.0.0.1/collector/g' current/config/dqmgui/server-conf-online.py
+
+$PWD/current/config/dqmgui/manage -f online start "I did read documentation"
+
 #su cmsusr -s /bin/bash "$@"
