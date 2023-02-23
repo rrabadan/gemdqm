6b03d394c5821a2d6468dd0399e4eae597543b65
Raul Rabadan <raul.iraq.rabadan.trejo@cern.ch>
2023-02-23 16:48:04 +0100

    GEMTestBeam workspace

--- dqmgui/python/gemtestbeam-layouts.py
+++ dqmgui/python/gemtestbeam-layouts.py
@@ -1,21 +1,9 @@
-# Generated automatically by dqmgui/gem_custom/fillLayouts.py 
-#   in https://github.com/quark2/deployment/tree/addingGem
-#   Recipe: 
-#     cd deployment/dqmgui
-#     python3 gem_custom/fillLayouts.py
 def GEMTestBeamLayout(i, p, *rows): i["GEMTestBeam/Layouts/" + p] = DQMItem(layout=rows)
 
 _GEMTestBeam_OFF_TWIKI = '<a href="https://twiki.cern.ch/twiki/bin/view/CMS/GEMPPDOfflineDQM">Link to TWiki</a>'
 
 ###############################################################################
-# Summary Map
-###############################################################################
-#GEMTestBeamLayout(dqmitems, '00 Summary Map',
-#          [{'path': 'GEMTestBeam/EventInfo/reportSummaryMap',
-#            'description': _GEMTestBeam_OFF_TWIKI}])
-
-###############################################################################
-# RecHit Occupancy
+# Digi Occupancy
 ###############################################################################
 GEMTestBeamLayout(dqmitems, 'Occupancy GE0',
           [{'path': 'GEMTestBeam/Digis/digi_occ_GE0',
--- dqmgui/python/workspaces-gem.py
+++ dqmgui/python/workspaces-gem.py
@@ -1,2 +1,1 @@
 server.workspace('DQMContent', 43, 'Muons', 'GEMTestBeam', '^GEMTestBeam/', '')
-server.workspace('DQMContent', 44, 'Muons', 'GE21QC8', '^GE21QC8/', '')
